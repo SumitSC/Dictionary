a96f8e104ae01369b784c6fb9b05a4a71e599b23
Sumit Chakraborty <84559344+SumitSC@users.noreply.github.com>
2021-05-21 14:35:01 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,31 @@
+import json
+from difflib import get_close_matches
+data=json.load(open("data.json"))
+def translate(w):
+    w=w.lower()
+    if w in data:
+        return data[w]
+    elif w.upper() in data:
+        return data[w.upper()]
+    elif w.title() in data:
+        return data[w.title()]
+    elif len(get_close_matches(w,data.keys()))>0:
+        yn=input("Did you mean %s ? Enter Y if yes or N if No: "%get_close_matches(w,data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w,data.keys())[0]]
+        elif yn == "N":
+            return "The word doesnt exist.Please double check it"
+        else:
+            return "We didnt understand your entry"
+    else:
+        return "The word doesnt exist.Please double check it."
+word=input("Enter word: ")
+output=translate(word)
+if type(output)==list:
+    for item in output:
+        print(item)
+else:
+    print(output)           
+
+        
+
